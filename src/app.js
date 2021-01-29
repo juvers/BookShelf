8fa9fd5ef714856da039c043d7d9b315aa9397c7
Jude kuti <judekuti@gmail.com>
2021-01-29 10:05:05 -0600

    Fix dynamic load errors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import {useAuth} from './context/auth-context'
 import {FullPageSpinner} from './components/lib'
 
 const AuthenticatedApp = React.lazy(() =>
-  import(/* webpackPrefetch: true */ './authenticated-app'),
+  import('./authenticated-app'),
 )
 const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'))
 
